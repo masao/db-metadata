db9ee0c6188251ef73bf591d50fbf5a3c170c1b1
yegusa <yuka@nier.go.jp>
2002-11-17 15:38:41 +0000

    dbname.db system.dbを作るように変更。

--- Makefile
+++ Makefile
@@ -1,5 +1,16 @@
-all: test
+INDEX = keyword.db dbname.db system.db
+
+all: $(INDEX) test
 
 # 簡単なテストを行う
 test: $(SCRIPTS)
 	perl -e 'use Test::Harness qw(&runtests $$verbose); $$verbose=1; runtests @ARGV;' tests/*.t
+
+$(INDEX):
+	rm -f $(INDEX)
+	for f in $(INDEX); do \
+	    ./mkindex.pl `basename $$f .db`; \
+	done;
+
+clean:
+	rm -f $(INDEX)
--- mkindex.pl
+++ mkindex.pl
@@ -9,28 +9,39 @@ require 'conf.pl';      # 設定内容を読み込む
 
 main();
 sub main {
-    my @files = util::pickup_files();
-    my %keyword = ();
-    tie(%keyword, 'DB_File', "keyword.db", O_CREAT|O_RDWR) ||
-        die "tie fail: keyword.db: $!";
+    if (defined($ARGV[0])) {
+	my @files = util::pickup_files();
+	
+	mkhash($ARGV[0], @files);
+    } else {
+	print "Usage: $0 target\n";
+	exit 1;
+    }
+}
+
+sub mkhash($$) {
+    my ($name, @files) = @_;
+    my %hash = ();
+    tie(%hash, 'DB_File', "$name.db", O_CREAT|O_RDWR) ||
+        die "tie fail: $name.db: $!";
 
     foreach my $file (@files) {
 	my $content = util::readfile("$conf::DATADIR/$file");
         my ($id) = get_tagvalues($content, "dbid");
-        my @keywords = get_tagvalues($content, "keyword");
-	# print "$id => @keywords\n";
-	foreach my $key (@keywords) {
+        my @values = get_tagvalues($content, $name);
+	# print "$id => @values\n";
+	foreach my $key (@values) {
 	    next unless defined $key;
-	    if (defined $keyword{$key}) {
-		$keyword{$key} .= ",$id";
+	    if (defined $hash{$key}) {
+		$hash{$key} .= ",$id";
 	    } else {
-		$keyword{$key} = $id;
+		$hash{$key} = $id;
 	    }
 	}
     }
-    foreach my $key (sort keys %keyword) {
-	print "$key => $keyword{$key}\n";
-    }
+#    foreach my $key (sort keys %hash) {
+#	print "$key => $hash{$key}\n";
+#    }
 }
 
 sub get_tagvalues($$) {
